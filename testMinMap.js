87a7fbdec70c3e5fd761b95991ef538b086178a5
yinhan <yinhan@uinnova.com>
2023-01-03 15:57:08 +0800

    ii

--- testMinMap.js
+++ testMinMap.js
@@ -1,12 +1,12 @@
-// /**
-//  * 说明：小地图控件
-//  * 难度：★★☆☆☆
-//  */
-// var app = new THING.App({
-//     url: 'https://www.thingjs.com/static/models/factory',
-//     background: '#000000',
-//     env: 'Seaside',
-// });
+/**
+ * 说明：小地图控件
+ * 难度：★★☆☆☆
+ */
+var app = new THING.App({
+    url: 'https://www.thingjs.com/static/models/factory',
+    background: '#000000',
+    env: 'Seaside',
+});
 // var control;
 // app.on('load', function () {
 //     // 创建提示
@@ -62,22 +62,38 @@
 //         });
 //     }
 // }
- // 加载地图
- var app = new THING.App();
- // 设置app背景为黑色
- app.background = [0, 0, 0];
- // 引用地图组件脚本
- THING.Utils.dynamicLoad(['./source/uearth.min.js'], function () {
-     app.create({
-       type: 'Map',
-       // 地图场景名：城市
-       url:"/city/cityBuilder/1042044",
-       // url: "./cityBuilder/1/map.bundle.json",
-       resourceConfig : {
-           resourcePrefix: '/Resources/CityMap/admin/1042044'
-       },
-       complete: function (event) {
-           console.log(event.object.userLayers.length);
-       }
-   });
- });
+//  // 加载地图
+//  var app = new THING.App();
+//  // 设置app背景为黑色
+//  app.background = [0, 0, 0];
+//  // 引用地图组件脚本
+//  THING.Utils.dynamicLoad(['./source/uearth.min.js'], function () {
+//      app.create({
+//        type: 'Map',
+//        // 地图场景名：城市
+//        url:"/city/cityBuilder/1042044",
+//        // url: "./cityBuilder/1/map.bundle.json",
+//        resourceConfig : {
+//            resourcePrefix: '/Resources/CityMap/admin/1042044'
+//        },
+//        complete: function (event) {
+//            console.log(event.object.userLayers.length);
+//        }
+//    });
+//  });
+// 离线开发引入森大屏图表脚本
+THING.Utils.dynamicLoad(['./static/release/thingjs-charts.umd.js'],
+async function () {
+    const scenebundle = THING.CHARTS.Utils.loadBundle(
+        '/Resources/BigScreen/admin/dbc717739dd046baa98e84a9ae05782c', // 前缀
+        {
+            container: document.querySelector('#div2d'), // 挂载节点
+            hide3D: true, // 隐藏场景内的3D，默认为false
+            hideCanvasBackground: true, // 画布背景色是否设置为透明的，默认false
+            //id: 96e8cb4172ff4fc7985e5ae09fe47439, //默认走bundle下的main，main不存在读id
+        }
+    )
+    await scenebundle.waitForComplete() // 等待场景加载完成
+    scenebundle.ui // ui实例
+}
+)
