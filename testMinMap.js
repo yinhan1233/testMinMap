1df5cfc76c22251948b5c0e9a3b3eb7c5c6afd3b
yinhan <yinhan@uinnova.com>
2023-01-03 16:50:59 +0800

    ee

--- testMinMap.js
+++ testMinMap.js
@@ -98,17 +98,33 @@ var app = new THING.App({
 // }
 // )
 
-// 创建APP对象
-var app = new THING.App();
-// 引用拓扑组件脚本
-THING.Utils.dynamicLoad(['./static/js/thing.diagram.min.js'], function () {
-    // 初始化拓扑场景
-    const graph = new THING.DIAGRAM.Graph({
-        container: 'div2d', // 容器元素 id
-        url: './diagrams/7917cc2512ae9612/topo.json' // 拓扑场景资源路径
-    });
-    // 视图加载完成
-    graph.on('load', () => {
-        console.log('加载完成');
-    });
-})
+// // 创建APP对象
+// var app = new THING.App();
+// // 引用拓扑组件脚本
+// THING.Utils.dynamicLoad(['./static/js/thing.diagram.min.js'], function () {
+//     // 初始化拓扑场景
+//     const graph = new THING.DIAGRAM.Graph({
+//         container: 'div2d', // 容器元素 id
+//         url: './diagrams/7917cc2512ae9612/topo.json' // 拓扑场景资源路径
+//     });
+//     // 视图加载完成
+//     graph.on('load', () => {
+//         console.log('加载完成');
+//     });
+// })
+// 离线开发引入森大屏图表脚本
+THING.Utils.dynamicLoad(['./static/release/thingjs-charts.umd.js'],
+async function () {
+    const scenebundle = THING.CHARTS.Utils.loadBundle(
+        '/Resources/BigScreen/admin/9a32fc758a44499d94a955680b30e369', // 前缀
+        {
+            container: document.querySelector('#div2d'), // 挂载节点
+            hide3D: true, // 隐藏场景内的3D，默认为false
+            hideCanvasBackground: true, // 画布背景色是否设置为透明的，默认false
+            //id: 9a32fc758a44499d94a955680b30e369, //默认走bundle下的main，main不存在读id
+        }
+    )
+    await scenebundle.waitForComplete() // 等待场景加载完成
+    scenebundle.ui // ui实例
+}
+)
