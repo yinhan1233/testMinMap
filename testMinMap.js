45dd10e5553653ef741dfa42e4fa9f31a41ad979
yinhan <yinhan@uinnova.com>
2023-01-04 17:15:40 +0800

    11

--- testMinMap.js
+++ testMinMap.js
@@ -128,23 +128,44 @@
 //     scenebundle.ui // ui实例
 // }
 // )
+//  // 加载地图
+//  var app = new THING.App();
+//  // 设置app背景为黑色
+//  app.background = [0, 0, 0];
+//  // 引用地图组件脚本
+//  THING.Utils.dynamicLoad(['/source/uearth.min.js'], function () {
+//      app.create({
+//        type: 'Map',
+//        // 地图场景名：唐山市路北区场景
+//        url:"/city/cityBuilder/1042044",
+//        // url: "./cityBuilder/1/map.bundle.json",
+//        resourceConfig : {
+//            resourcePrefix: '/Resources/CityMap/admin/1042044'
+//        },
+//        complete: function (event) {
+//            console.log(event.object.userLayers.length);
+//        }
+//    });
+//  });
  // 加载地图
  var app = new THING.App();
  // 设置app背景为黑色
  app.background = [0, 0, 0];
  // 引用地图组件脚本
  THING.Utils.dynamicLoad(['/source/uearth.min.js'], function () {
+     let defaultPath = "/Resources/CityMap/admin/1042044";
      app.create({
        type: 'Map',
        // 地图场景名：唐山市路北区场景
-       url:"/city/cityBuilder/1042044",
+       url:defaultPath + '/map.bundle.json',
        // url: "./cityBuilder/1/map.bundle.json",
        resourceConfig : {
-           resourcePrefix: '/Resources/CityMap/admin/1042044'
+           resourcePrefix: defaultPath
        },
        complete: function (event) {
            console.log(event.object.userLayers.length);
        }
    });
  });
+
 
