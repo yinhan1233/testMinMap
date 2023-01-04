c44291641e51676158bc3b98e6e82a38a5f25d99
wangyuyan@uinnova.com <wangyuyan@uinnova.com>
2023-01-04 17:59:53 +0800

    fix()

--- testMinMap.js
+++ testMinMap.js
@@ -1,171 +1,21 @@
-/**
- * 说明：小地图控件
- * 难度：★★☆☆☆
- */
-// var app = new THING.App({
-//     url: 'https://www.thingjs.com/static/models/factory',
-//     background: '#000000',
-//     env: 'Seaside',
-// });
-// var control;
-// app.on('load', function () {
-//     // 创建提示
-//     initThingJsTip("本例程展示了平台内置的小地图控件，点击左侧按钮进行体验");
-//     new THING.widget.Button('添加控件', add_control);
-//     new THING.widget.Button('重置', remove_control);
-// });
-
-// /**
-//  * 添加控件
-//  */
-// function add_control() {
-//     if (control) return;
-//     // 摄像机飞行到某位置
-//     app.camera.flyTo({
-//         'position': [29.94499415627374, 30.51853855043671, 27.57498676778577],
-//         'target': [4.330696213040164, -0.6832814144239165, -8.422960580769757],
-//         'time': 1000,
-//         'complete': function () {
-//             control = new THING.MiniMapControl({
-//                 width: 200,
-//                 height: 200,
-//                 position: THING.CornerType.LeftBottom,
-//                 opacity: 0.8,
-//                 scale: 2,
-//                 center: [0, -10],
-//                 cameraViewImg: './/static/images/minimap1.png',
-//                 cameraCenterImg: './/static/images/minimap0.png',
-//                 // hasClose: true, // 是否有关闭按钮（默认没有）当点击关闭按钮时，小地图 enable 为 false
-//                 // closeBtnImg: 'https://www.thingjs.com/static/images/minimap2.png'
-//             })
-//             // 添加小地图控件
-//             app.addControl(control);
-//         }
-//     });
-// }
-
-// /**
-//  * 删除控件
-//  */
-// function remove_control() {
-//     if (control) {
-//         // 摄像机飞行到某位置
-//         app.camera.flyTo({
-//             'position': [36.013, 42.67799999999998, 61.72399999999999],
-//             'target': [1.646, 7.891, 4.445],
-//             'time': 1000,
-//             'complete': function () {
-//                 app.removeControl(control);
-//                 control = null;
-//                 // control.minimap.enable = !control.minimap.enable;  // 显示、隐藏小地图
-//             }
-//         });
-//     }
-// }
-//  // 加载地图
-//  var app = new THING.App();
-//  // 设置app背景为黑色
-//  app.background = [0, 0, 0];
-//  // 引用地图组件脚本
-//  THING.Utils.dynamicLoad(['./source/uearth.min.js'], function () {
-//      app.create({
-//        type: 'Map',
-//        // 地图场景名：城市
-//        url:"/city/cityBuilder/1042044",
-//        // url: "./cityBuilder/1/map.bundle.json",
-//        resourceConfig : {
-//            resourcePrefix: '/Resources/CityMap/admin/1042044'
-//        },
-//        complete: function (event) {
-//            console.log(event.object.userLayers.length);
-//        }
-//    });
-//  });
-// // 离线开发引入森大屏图表脚本
-// THING.Utils.dynamicLoad(['./static/release/thingjs-charts.umd.js'],
-// async function () {
-//     const scenebundle = THING.CHARTS.Utils.loadBundle(
-//         '/Resources/BigScreen/admin/dbc717739dd046baa98e84a9ae05782c', // 前缀
-//         {
-//             container: document.querySelector('#div2d'), // 挂载节点
-//             hide3D: true, // 隐藏场景内的3D，默认为false
-//             hideCanvasBackground: true, // 画布背景色是否设置为透明的，默认false
-//             //id: 96e8cb4172ff4fc7985e5ae09fe47439, //默认走bundle下的main，main不存在读id
-//         }
-//     )
-//     await scenebundle.waitForComplete() // 等待场景加载完成
-//     scenebundle.ui // ui实例
-// }
-// )
-
-// // 创建APP对象
-// var app = new THING.App();
-// // 引用拓扑组件脚本
-// THING.Utils.dynamicLoad(['./static/js/thing.diagram.min.js'], function () {
-//     // 初始化拓扑场景
-//     const graph = new THING.DIAGRAM.Graph({
-//         container: 'div2d', // 容器元素 id
-//         url: './diagrams/7917cc2512ae9612/topo.json' // 拓扑场景资源路径
-//     });
-//     // 视图加载完成
-//     graph.on('load', () => {
-//         console.log('加载完成');
-//     });
-// })
-// 离线开发引入森大屏图表脚本
-// THING.Utils.dynamicLoad(['./static/release/thingjs-charts.umd.js'],
-// async function () {
-//     const scenebundle = THING.CHARTS.Utils.loadBundle(
-//         '/Resources/BigScreen/admin/96e8cb4172ff4fc7985e5ae09fe47439', // 前缀
-//         {
-//             container: document.querySelector('#div2d'), // 挂载节点
-//             hide3D: true, // 隐藏场景内的3D，默认为false
-//             hideCanvasBackground: true, // 画布背景色是否设置为透明的，默认false
-//             //id: 96e8cb4172ff4fc7985e5ae09fe47439, //默认走bundle下的main，main不存在读id
-//         }
-//     )
-//     await scenebundle.waitForComplete() // 等待场景加载完成
-//     scenebundle.ui // ui实例
-// }
-// )
-//  // 加载地图
-//  var app = new THING.App();
-//  // 设置app背景为黑色
-//  app.background = [0, 0, 0];
-//  // 引用地图组件脚本
-//  THING.Utils.dynamicLoad(['/source/uearth.min.js'], function () {
-//      app.create({
-//        type: 'Map',
-//        // 地图场景名：唐山市路北区场景
-//        url:"/city/cityBuilder/1042044",
-//        // url: "./cityBuilder/1/map.bundle.json",
-//        resourceConfig : {
-//            resourcePrefix: '/Resources/CityMap/admin/1042044'
-//        },
-//        complete: function (event) {
-//            console.log(event.object.userLayers.length);
-//        }
-//    });
-//  });
  // 加载地图
  var app = new THING.App();
  // 设置app背景为黑色
  app.background = [0, 0, 0];
  // 引用地图组件脚本
  THING.Utils.dynamicLoad(['/source/uearth.min.js'], function () {
-     let defaultPath = '/Resources/CityMap/admin/1042044';
+     let defaultPath = '/Resources/CityMap/admin/1064811';
      app.create({
        type: 'Map',
        // 地图场景名：唐山市路北区场景
-       url:'/Resources/CityMap/admin/1042044/map.bundle.json',
+       url:defaultPath +'/map.bundle.json',
        // url: "./cityBuilder/1/map.bundle.json",
        resourceConfig : {
-           resourcePrefix: '/Resources'
+           resourcePrefix: defaultPath
        },
        complete: function (event) {
            console.log(event.object.userLayers.length);
        }
    });
  });
-
 
