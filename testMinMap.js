20e9566fec3586d285193b4e69230275c10fe708
yinhan <yinhan@uinnova.com>
2023-01-04 17:18:18 +0800

    3333

--- testMinMap.js
+++ testMinMap.js
@@ -153,14 +153,14 @@
  app.background = [0, 0, 0];
  // 引用地图组件脚本
  THING.Utils.dynamicLoad(['/source/uearth.min.js'], function () {
-     let defaultPath = "/Resources/CityMap/admin/1042044";
+     let defaultPath = '/Resources/CityMap/admin/1042044';
      app.create({
        type: 'Map',
        // 地图场景名：唐山市路北区场景
-       url:defaultPath + '/map.bundle.json',
+       url:'/Resources/CityMap/admin/1042044/map.bundle.json',
        // url: "./cityBuilder/1/map.bundle.json",
        resourceConfig : {
-           resourcePrefix: defaultPath
+           resourcePrefix: '/Resources/CityMap/admin/1042044'
        },
        complete: function (event) {
            console.log(event.object.userLayers.length);
