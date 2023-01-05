f55a7c6e2bec51bbaf3f7dd8e51a3de3952e17b5
yinhan <yinhan@uinnova.com>
2023-01-05 16:03:26 +0800

    f

--- testMinMap.js
+++ testMinMap.js
@@ -8,8 +8,8 @@
     app.create({
         type: 'Map',
         // 地图场景名：唐山市路北区场景
-        url: defaultPath + '/map.bundle.json',
-        // url: "./cityBuilder/1/map.bundle.json",
+        // url: defaultPath + '/map.bundle.json',
+        url: "./city/cityBuilder/1042044/",
        resourceConfig : {
             resourcePrefix: defaultPath
         },
