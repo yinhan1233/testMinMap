876f2ca7567fff2d02a4c1c90f14c15e200ac7d8
wangyuyan@uinnova.com <wangyuyan@uinnova.com>
2023-01-05 15:38:14 +0800

    fix()

--- testMinMap.js
+++ testMinMap.js
@@ -32,3 +32,20 @@ THING.Utils.dynamicLoad(['./static/js/thing.diagram.min.js'], function () {
         console.log('加载完成');
     });
 })
+
+// 离线开发引入森大屏图表脚本
+THING.Utils.dynamicLoad(['./static/release/thingjs-charts.umd.js'],
+async function () {
+    const scenebundle = THING.CHARTS.Utils.loadBundle(
+        '/Resources/BigScreen/admin/f232639c3439491e861cc7823f6ef38d', // 前缀
+        {
+            container: document.querySelector('#div2d'), // 挂载节点
+            hide3D: true, // 隐藏场景内的3D，默认为false
+            hideCanvasBackground: true, // 画布背景色是否设置为透明的，默认false
+            //id: f232639c3439491e861cc7823f6ef38d, //默认走bundle下的main，main不存在读id
+        }
+    )
+    await scenebundle.waitForComplete() // 等待场景加载完成
+    scenebundle.ui // ui实例
+}
+)
