3787375fb81d79d41938149e559b77c3284ec247
yinhan <yinhan@uinnova.com>
2023-01-05 16:36:45 +0800

    2222222

--- testMinMap.js
+++ testMinMap.js
@@ -1,20 +1,20 @@
- // 加载地图
+// 加载地图
  var app = new THING.App();
  // 设置app背景为黑色
  app.background = [0, 0, 0];
  // 引用地图组件脚本
  THING.Utils.dynamicLoad(['/source/uearth.min.js'], function () {
-     let defaultPath = '/Resources/CityMap/admin/1042044';
-    app.create({
-        type: 'Map',
-        // 地图场景名：唐山市路北区场景
-        url: defaultPath + '/map.bundle.json',
-        // url: "./city/cityBuilder/1042044/",
+     let defaultPath = '/Resources/CityMap/admin/1043850';
+     app.create({
+       type: 'Map',
+       // 地图场景名：路北区
+       url: defaultPath + '/map.bundle.json',
+       // url: "./cityBuilder/1/map.bundle.json",
        resourceConfig : {
-            resourcePrefix: defaultPath
-        },
-        complete: function (event) {
-            console.log(event.object.userLayers.length);
-        }
-    });
-})
+           resourcePrefix: defaultPath
+       },
+       complete: function (event) {
+           console.log(event.object.userLayers.length);
+       }
+   });
+ });
