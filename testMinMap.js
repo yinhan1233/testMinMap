8b7e72adc82566c691e7b7363f10dc54a535711f
yinhan <yinhan@uinnova.com>
2023-01-10 15:55:00 +0800

    11111111

--- testMinMap.js
+++ testMinMap.js
@@ -1,16 +1,15 @@
-// 离线开发引入森大屏图表脚本
-THING.Utils.dynamicLoad(['/static/release/thingjs-charts.umd.js'],
-async function () {
-    const scenebundle = THING.CHARTS.Utils.loadBundle(
-        '/Resources/BigScreen/admin/96e8cb4172ff4fc7985e5ae09fe47439', // 前缀
-        {
-            container: document.querySelector('#div2d'), // 挂载节点
-            hide3D: true, // 隐藏场景内的3D，默认为false
-            hideCanvasBackground: true, // 画布背景色是否设置为透明的，默认false
-            //id: 96e8cb4172ff4fc7985e5ae09fe47439, //默认走bundle下的main，main不存在读id
-        }
-    )
-    await scenebundle.waitForComplete() // 等待场景加载完成
-    scenebundle.ui // ui实例
-}
-)
+// 创建APP对象
+var app = new THING.App();
+// 引用拓扑组件脚本
+THING.Utils.dynamicLoad(['/static/js/thing.diagram.min.js'], function () {
+    // 初始化拓扑场景
+    const graph = new THING.DIAGRAM.Graph({
+        container: 'div2d', // 容器元素 id
+        url: './diagrams/9d124b930cefdf59/topo.json', // 拓扑场景资源路径
+        resourceRootPath: '/Resources/topo/admin/9d124b930cefdf59'
+    });
+    // 视图加载完成
+    graph.on('load', () => {
+        console.log('加载完成');
+    });
+})
