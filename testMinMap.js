1dbc2b245911cbd17f217eb451b94c2b73d6eadf
yinhan <yinhan@uinnova.com>
2023-01-10 10:30:42 +0800

    222222

--- testMinMap.js
+++ testMinMap.js
@@ -33,25 +33,46 @@
 //         console.log('加载完成');
 //     });
 // })
- // 加载地图
- var app = new THING.App();
- // 设置app背景为黑色
- app.background = [0, 0, 0];
- // 引用地图组件脚本
- THING.Utils.dynamicLoad(['/source/uearth.min.js'], function () {
-     let defaultPath = '/Resources/CityMap/admin/1043850';
-     app.create({
-       type: 'Map',
-       // 地图场景名：路北区
-       url: defaultPath + '/map.bundle.json',
-       // url: "./cityBuilder/1/map.bundle.json",
-       resourceConfig : {
-           resourcePrefix: defaultPath
-       },
-       complete: function (event) {
-           console.log(event.object.userLayers.length);
-       }
-   });
- });
+//  // 加载地图
+//  var app = new THING.App();
+//  // 设置app背景为黑色
+//  app.background = [0, 0, 0];
+//  // 引用地图组件脚本
+//  THING.Utils.dynamicLoad(['/source/uearth.min.js'], function () {
+//      let defaultPath = '/Resources/CityMap/admin/1043850';
+//      app.create({
+//        type: 'Map',
+//        // 地图场景名：路北区
+//        url: defaultPath + '/map.bundle.json',
+//        // url: "./cityBuilder/1/map.bundle.json",
+//        resourceConfig : {
+//            resourcePrefix: defaultPath
+//        },
+//        complete: function (event) {
+//            console.log(event.object.userLayers.length);
+//        }
+//    });
+//  });
+
+  // 加载地图
+  var app = new THING.App();
+  // 设置app背景为黑色
+  app.background = [0, 0, 0];
+  // 引用地图组件脚本
+  THING.Utils.dynamicLoad(['/source/uearth.min.js'], function () {
+      let defaultPath = '/Resources/CityMap/xiaoli/1043850';
+      app.create({
+        type: 'Map',
+        // 地图场景名：路北区
+        url: defaultPath + '/map.bundle.json',
+        // url: "./cityBuilder/1/map.bundle.json",
+        resourceConfig : {
+            resourcePrefix: defaultPath
+        },
+        complete: function (event) {
+            console.log(event.object.userLayers.length);
+        }
+    });
+  });
 
 
