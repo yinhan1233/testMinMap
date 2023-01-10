b21fca336c52e095d0ecfbdff23eb2eb1de671be
yinhan <yinhan@uinnova.com>
2023-01-10 13:47:30 +0800

    2222222

--- testMinMap.js
+++ testMinMap.js
@@ -1,18 +1,16 @@
-
 // 离线开发引入森大屏图表脚本
 THING.Utils.dynamicLoad(['/static/release/thingjs-charts.umd.js'],
 async function () {
     const scenebundle = THING.CHARTS.Utils.loadBundle(
-        '/Resources/BigScreen/admin/664253608d8848cc9e5ce8cd20ee97d5', // 前缀
+        '/Resources/BigScreen/admin/96e8cb4172ff4fc7985e5ae09fe47439', // 前缀
         {
             container: document.querySelector('#div2d'), // 挂载节点
             hide3D: true, // 隐藏场景内的3D，默认为false
             hideCanvasBackground: true, // 画布背景色是否设置为透明的，默认false
-            //id: 664253608d8848cc9e5ce8cd20ee97d5, //默认走bundle下的main，main不存在读id
+            //id: 96e8cb4172ff4fc7985e5ae09fe47439, //默认走bundle下的main，main不存在读id
         }
     )
     await scenebundle.waitForComplete() // 等待场景加载完成
     scenebundle.ui // ui实例
 }
 )
-
