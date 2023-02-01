8fa67b18e86eb2d70595626a2837915f2d2c6d40
yinhan <yinhan@uinnova.com>
2023-02-01 17:07:20 +0800

    222222

--- testMinMap.js
+++ testMinMap.js
@@ -21,27 +21,41 @@
 // //         });
 // //     }
 // // )
- // 加载地图
- var app = new THING.App();
- // 设置app背景为黑色
- app.background = [0, 0, 0];
- // 引用地图组件脚本
- THING.Utils.dynamicLoad(['/source/uearth.min.js'], function () {
-     let defaultPath = '/Resources/CityMap/admin/43933';
-     app.create({
-       type: 'Map',
-       // 地图场景名：西城区基础图层
-       url: defaultPath + '/map.bundle.json',
-       // url: "./cityBuilder/1/map.bundle.json",
-       resourceConfig : {
-           resourcePrefix: defaultPath
-       },
-       complete: function (event) {
-           console.log(event.object.userLayers.length);
-       }
-   });
- });
-
+//  // 加载地图
+//  var app = new THING.App();
+//  // 设置app背景为黑色
+//  app.background = [0, 0, 0];
+//  // 引用地图组件脚本
+//  THING.Utils.dynamicLoad(['/source/uearth.min.js'], function () {
+//      let defaultPath = '/Resources/CityMap/admin/43933';
+//      app.create({
+//        type: 'Map',
+//        // 地图场景名：西城区基础图层
+//        url: defaultPath + '/map.bundle.json',
+//        // url: "./cityBuilder/1/map.bundle.json",
+//        resourceConfig : {
+//            resourcePrefix: defaultPath
+//        },
+//        complete: function (event) {
+//            console.log(event.object.userLayers.length);
+//        }
+//    });
+//  });
+// 创建APP对象
+var app = new THING.App();
+// 引用拓扑组件脚本
+THING.Utils.dynamicLoad(['/static/js/thing.diagram.min.js'], function () {
+    // 初始化拓扑场景
+    const graph = new THING.DIAGRAM.Graph({
+        container: 'div2d', // 容器元素 id
+        url: './diagrams/41d8aad40177a399/topo.json', // 拓扑场景资源路径
+        resourceRootPath: '/Resources/topo/admin/41d8aad40177a399'
+    });
+    // 视图加载完成
+    graph.on('load', () => {
+        console.log('加载完成');
+    });
+})
 
 
 
