f7009cd36abee0eb800add81ae3f045474d847ef
yinhan <yinhan@uinnova.com>
2023-02-07 17:33:31 +0800

    111

--- testMinMap.js
+++ testMinMap.js
@@ -5,23 +5,44 @@ var app = new THING.App({ resourceLibraryUrl: "./",
 });
 
 
-// 引入一键图表脚本
-THING.Utils.dynamicLoad(['./static/ScenePreview/chart/PreviewChartControl.js'], function () {
-    // 创建图表
-    new PreviewChartControl({
-        url: '/api/chart/63870ddd05108267c4a1b6df',
-        tag: '1',
-        isLoad: true,  // 加载页，默认为true
-        isApplyBackground: false,  // 是否应用图表背景，启用该参数会替换app.background的值，默认false
-        autoResize: {  // 图表自适应
-           enable: false,  // 自适应图表分辨率，默认为false
-           orgin: 'center center'  // 自适应的基准点，第一个参数取值为:left、center、right，第二个参数取值为:top、center、bottom，默认值为“center center”
-        },
-        complete: function(){
+// // 引入一键图表脚本
+// THING.Utils.dynamicLoad(['./static/ScenePreview/chart/PreviewChartControl.js'], function () {
+//     // 创建图表
+//     new PreviewChartControl({
+//         url: '/api/chart/63870ddd05108267c4a1b6df',
+//         tag: '1',
+//         isLoad: true,  // 加载页，默认为true
+//         isApplyBackground: false,  // 是否应用图表背景，启用该参数会替换app.background的值，默认false
+//         autoResize: {  // 图表自适应
+//            enable: false,  // 自适应图表分辨率，默认为false
+//            orgin: 'center center'  // 自适应的基准点，第一个参数取值为:left、center、right，第二个参数取值为:top、center、bottom，默认值为“center center”
+//         },
+//         complete: function(){
            
-        }
-    });
-})
+//         }
+//     });
+// })
+ // 加载地图
+ var app = new THING.App();
+ // 设置app背景为黑色
+ app.background = [0, 0, 0];
+ // 引用地图组件脚本
+ THING.Utils.dynamicLoad(['/source/uearth.min.js'], function () {
+     let defaultPath = '/Resources/CityMap/admin/131982';
+     app.create({
+       type: 'Map',
+       // 地图场景名：mapTest
+       url: defaultPath + '/map.bundle.json',
+       // url: "./cityBuilder/1/map.bundle.json",
+       resourceConfig : {
+           resourcePrefix: defaultPath
+       },
+       complete: function (event) {
+           console.log(event.object.userLayers.length);
+       }
+   });
+ });
+
 
 
 
