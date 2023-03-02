a9ced4c7241c1e77c924e9666376efe6b45a9992
yinhan <yinhan@uinnova.com>
2023-03-02 13:48:08 +0800

    11111111

--- testMinMap.js
+++ testMinMap.js
@@ -1,55 +1,82 @@
-
-// /**
-// * 名称：极光地图
-// * 说明：当前效果模板仅适用于渲染地图效果
-// */
-// // 加载地图
-// var app = new THING.App();
-// // 设置app背景为黑色
-// app.background = [0, 0, 0];
-// // 引用地图组件脚本
-// THING.Utils.dynamicLoad(['/source/uearth.min.js'], function () {
-//     // 渲染模板的url
-//     const mapUrl = '/Resources/EffectTemplate/admin/3940';
-//     app.create({
-//         type: 'Map',
-//         //此处可更换为CityBuilder生成的地图URL链接
-//         url: 'https://www.thingjs.com/citybuilder_console/mapProject/config/TVRJeE9EQTFDaXR5QnVpbGRlckAyMDE5/true',
-//         complete: function (event) {
-//             // 进行效果模板渲染
-//             const resourceConfig = {
-//                 resourcePrefix: mapUrl
-//             };
-//             CMAP.Util.applyTemplate(mapUrl+'/map.json', resourceConfig);
-//         }
-//     });
-// });
-// 创建Thing
+// 创建线标记
 var app = new THING.App();
-var obj = app.create({
-    type: 'Thing',
-    name: '桌子', 
-    url:'/Resources/Model/admin/4ce8d7c7045b4782b5caade8db35cda8',
-    position: [0, 0, 0],
-    angle: 0,
-    complete: function () {
-        console.log('thing created: ' + this.id);
-    }
-});
-// 引入一键图表脚本
-THING.Utils.dynamicLoad(['/static/ScenePreview/chart/PreviewChartControl.js'], function () {
-    // 创建图表
-    new PreviewChartControl({
-        url: '/api/chart/63870ddd05108267c4a1b6df',
-        tag: '1',
-        isLoad: true,  // 加载页，默认为true
-        isApplyBackground: false,  // 是否应用图表背景，启用该参数会替换app.background的值，默认false
-        autoResize: {  // 图表自适应
-            enable: false,  // 自适应图表分辨率，默认为false
-            orgin: 'center center'  // 自适应的基准点，第一个参数取值为:left、center、right，第二个参数取值为:top、center、bottom，默认值为“center center”
+app.background = [0, 0, 0];
+console.log('测试打包票文件');
+console.log('测试打包票文件');
+console.log('测试打包票文件');
+console.log('测试打包票文件');
+console.log('测试打包票文件');
+console.log('测试打包票文件');
+console.log('测试打包票文件');
+console.log('测试打包票文件');
+// 引用地图组件脚本
+THING.Utils.dynamicLoad([
+    // 引用地图组件脚本（加载地图时进行使用）
+    '/source/uearth.min.js',
+    // 引入配饰库
+    '/static/attachment/js/objectdecorator.min.js',
+], function () {
+    // 创建一个地图
+  var map = app.create({
+        type: 'Map',
+        attribution: '高德',
+        style: {
+            night: false,
         },
-        complete: function () {
-
-        }
     });
-})
+    // 创建一个瓦片图层
+    var tileLayer1 = app.create({
+        type: 'TileLayer',
+        name: '卫星影像图层',
+        url: 'https://webst0{1,2,3,4}.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}',
+        style: {
+            template: CMAP.TileLayerStyle.DARKBLUE, // 设置瓦片图层的样式为DARKBLUE
+        },
+    });
+    // 将瓦片图添加到底图图层中
+    map.addLayer(tileLayer1);
+
+    // 摄像机飞行到某位置
+    app.camera.flyTo({
+        position: [2182239.4599693236, 4094671.06166381, 4381494.033699439],
+        target: [2177632.6085063685, 4098068.082322953, 4375230.0756663885],
+        time: 2000,
+        complete: function () {
+            addLayer();
+        },
+    });
+
+    /**
+     * 创建图层并修改样式
+     */
+    function addLayer() {
+        $.ajax({
+            type: 'GET',
+            url: '/uearth/uGeo/wangjingRoad.geojson',
+            dataType: 'json',
+            success: function (data) {
+            let acssessorysrc, objs_pathline = [], objsPoints = [];
+                // 创建标记实例
+                let atm = new Attachment();
+                let routeLineData = data
+                let baseObj = app.create({
+                    type: 'BaseObject',
+                    name: 'test-0'
+                });
+                baseObj.position = CMAP.Util.convertLonlatToWorld(routeLineData.features[0].geometry.coordinates[0][0]);
+                baseObj.geojson = routeLineData;
+                objs_pathline.push(baseObj);
+                // 引用标记资源路径
+                acssessorysrc = '/Resources/bundle/admin/2124-3144';
+                /**
+                 * 创建标记
+                 * @param {'地图'，'园区'，'室内'} sceneType - 使用环境
+                 */
+                atm.create(objs_pathline, acssessorysrc, { sceneType: '地图' }).then(function (obj) {
+                    console.log("acssessorysrc",acssessorysrc)
+                });
+            },
+        });
+    }
+}
+);
