998b9b3296450f0ff224e016a137df8ecf497e28
yinhan <yinhan@uinnova.com>
2023-05-30 15:34:22 +0800

    更新

--- case_new.js
+++
@@ -1,9 +0,0 @@
-// // 离线开发引入园区场景
-// var app = new THING.App({
-//   url: '/api/scene/6065231662535370718'  // 场景地址
-// });
-
-// 离线开发引入园区场景
-var app = new THING.App({
-  url: '/api/scene/7066231681372272419'  // 场景地址
-});
